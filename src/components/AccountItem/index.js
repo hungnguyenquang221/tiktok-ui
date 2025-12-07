5c9e3d2d41073a40e3c4416d0c6ac37d3b8a221d
hungnguyenquang221 <95635825+hungnguyenquang221@users.noreply.github.com>
2025-12-07 13:46:04 +0700

    push

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,21 @@
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/f9ce554d025b4706351928dd399de887~tplv-tiktokx-cropcenter:720:720.jpeg?dr=14579&refresh_token=08db7a73&x-expires=1765166400&x-signature=SP4FwjysrM%2BCfrYq7I0t54DRlSg%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=sg1" alt="Hoa" />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Hoa Dep Trai</span>
+                    <FontAwesomeIcon className={cx('check')} icon="fa-solid fa-circle-check" />
+                </h4>
+                <span className={cx('username')}>hoadptrai</span>
+            </div>
+
+        </div>
+    );
+}
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,23 +1,60 @@
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
 const cx = classNames.bind(styles);
 function Header() {
-    return <header className= {cx('wrapper')}>
+    const [searchResult, setSearchResult] = useState([]);
+    useEffect(() => {
+        // fake API
+        setTimeout(() => {
+            setSearchResult([1, 2, 3]);
+        }, 0);
+    }, []);
+    return  <header className= {cx('wrapper')}>
         <div className= {cx('inner')}>
-                <img src= {images.logo} alt="Tiktok" />
+            <img src= {images.logo} alt="Tiktok" />
+            <Tippy
+                interactive
+                visible={searchResult.length > 0}
+                placement="bottom"
+                offset={[0, 8]}
+                appendTo={() => document.body}
+                render={(attrs) => (
+                    <div 
+                        className={cx('search-result')} 
+                        tabIndex={-1} 
+                        {...attrs}
+                        style={{ zIndex: 99999 }}
+                    >
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            <AccountItem />
+                            <AccountItem />
+                            <AccountItem />
+                            <AccountItem />
+                            <AccountItem />
+                        </PopperWrapper>
+                    </div>
+                )}
+            >
             <div className = {cx('search')}>
                 <input placeholder="Search accounts and videos" spellCheck = {false}/>
                 <button className={cx('clear')}>
                     <FontAwesomeIcon icon={faCircleXmark} />
                 </button>
                 <FontAwesomeIcon className= {cx('loading')} icon={faSpinner} />
-                <button className={cx('search-btn')}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </button>
+                
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
             </div>
+            </Tippy>
             <div className={cx('actions')}>
                 <button className={cx('action-btn')}>Upload</button>
                 <button className={cx('action-btn', 'primary')}>Log in</button>
