d63639d6fd7c43f9b8531c46a1a86c0a445310be
hungnguyenquang221 <95635825+hungnguyenquang221@users.noreply.github.com>
2025-12-11 11:26:20 +0700

    logic search data

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,20 +2,21 @@ import classNames from "classnames/bind";
 import styles from './AccountItem.module.scss';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "~/components/Image";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { Link } from 'react-router-dom';
 const cx = classNames.bind(styles);
-function AccountItem() {
+function AccountItem({data}) {
     return (
-        <div className={cx('wrapper')}>
-            <Image className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/f9ce554d025b4706351928dd399de887~tplv-tiktokx-cropcenter:720:720.jpeg?dr=14579&refresh_token=08db7a73&x-expires=1765166400&x-signature=SP4FwjysrM%2BCfrYq7I0t54DRlSg%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=sg1" alt="Hoa" />
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+            <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
-                    <span>Hoa Dep Trai</span>
-                    <FontAwesomeIcon className={cx('check')} icon="fa-solid fa-circle-check" />
+                    <span>{data.full_name}</span>
+                    {data.tick && <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />}
                 </h4>
                 <span className={cx('username')}>hoadptrai</span>
             </div>
-
-        </div>
+        </Link>
     );
 }
 export default AccountItem;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ import Home from "../pages/Home";
 import Following from "../pages/Following";
 import Upload from "~/pages/Upload";
 import Search from "~/pages/Search";
+import Profile from "~/pages/Profile";
 const publicRoutes = [
     {path: '/', component: Home},
     {path: '/following', component: Following},
+    { path: '/@:nickname', component: Profile },
     {path: '/upload', component: Upload, layout: HeaderOnly},
     {path: '/search', component: Search, layout: null},
 ];
