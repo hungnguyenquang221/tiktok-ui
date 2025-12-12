962ac3824dbc93d41007c0ab1c57cbf11fbfcd91
hungnguyenquang221 <95635825+hungnguyenquang221@users.noreply.github.com>
2025-12-12 13:47:45 +0700

    config build code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import { publicRoutes } from "./routes";
-import {DefaultLayout} from "./components/Layout";
+import DefaultLayout from "~/layouts";
 import { Fragment } from "react";
 function App() {
   return (
--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -18,7 +18,7 @@ faEllipsisVertical,
   import Button from "~/components/Button";
   import Menu from "~/components/Popper/Menu";
   import { Link } from "react-router";
-  import routesConfig from '~/config/routes'
+  import config from '~/config'
 
 import Search from "../Search";
 const cx = classNames.bind(styles);
@@ -93,7 +93,7 @@ function Header() {
   return (
     <header className={cx("wrapper")}>
       <div className={cx("inner")}>
-        <Link to={routesConfig.home} className={cx('logo-link')}>
+        <Link to={config.routes.home} className={cx('logo-link')}>
           <img src={images.logo} alt="Tiktok" />
         </Link>
         <Search />
